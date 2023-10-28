1b5a2b5743b0620a8b73506a7ed4ccf68e680f73
jaylu11 <lushuyuan1@hotmail.com>
2023-10-28 15:32:57 +0000

    demo

---
+++ contracts/Raffle.sol
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+error Raffle__NotEnoughETHEntered();
+error Raffle__TransferFailed();
+
+contract Raffle is VRFConsumerBaseV2 {
+    /*state Variables*/
+    uint private immutable i_entranceFee;
+    address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint64 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    //Lottery Variables
+    address private s_recenteWinner;
+
+    /*Events*/
+    event RaffleEnter(address indexed player);
+    event RequestedRaffleWinnerId(uint indexed requestId);
+    event WinnerPicked(address recentWinner);
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
+    }
+
+    constructor(
+        address vrfCoordinatorV2,
+        uint entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+        i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+    }
+
+    function requestRandomWiner() external {
+        uint requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        emit RequestedRaffleWinnerId(requestId);
+    }
+
+    function fulfillRandomWords(
+        uint256 /*requestId*/,
+        uint256[] memory randomWords
+    ) internal override {
+        uint indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        s_recenteWinner = recentWinner;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+        emit WinnerPicked(recentWinner);
+    }
+
+    function getEntranceFee() public view returns (uint) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint index) public view returns (address) {
+        return s_players[index];
+    }
+
+    function getRencentWinner() public view returns (address) {
+        return s_recenteWinner;
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@
  * @Author: jaylu11 lushuyuan1@hotmail.com
  * @Date: 2023-10-27 15:13:30
  * @LastEditors: jaylu11 lushuyuan1@hotmail.com
- * @LastEditTime: 2023-10-27 15:25:54
+ * @LastEditTime: 2023-10-28 13:57:48
  * @FilePath: /hardhat-smartcontract-lottery/hardhat.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,5 +15,5 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 module.exports = {
-    solidity: "0.8.22",
+    solidity: "0.8.21",
 }
