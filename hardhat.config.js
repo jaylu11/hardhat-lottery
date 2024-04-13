10448d216760939a8859992090b9d1bec2e31667
jaylu11 <lushuyuan1@hotmail.com>
2024-04-13 06:51:33 +0000

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,3 @@
-/*
- * @Author: jaylu11 lushuyuan1@hotmail.com
- * @Date: 2023-10-27 15:13:30
- * @LastEditors: jaylu11 lushuyuan1@hotmail.com
- * @LastEditTime: 2023-10-31 12:38:07
- * @FilePath: /hardhat-smartcontract-lottery/hardhat.config.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-/** @type import('hardhat/config').HardhatUserConfig */
 require("@nomiclabs/hardhat-waffle")
 require("@nomiclabs/hardhat-etherscan")
 require("hardhat-deploy")
